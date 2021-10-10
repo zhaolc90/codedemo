b7acbf89c8d4fde3ca88154bcd99f3aaff0cf96c
zhaolc90 <ytmssr@outlook.com>
2021-10-10 17:28:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+global.TextEncoder = require("util").TextEncoder;
+global.TextDecoder = require("util").TextDecoder;
+
 var express = require("express"),
   config = require("./config"),
   spdy = require("spdy"),
